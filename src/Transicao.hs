9052adc394ecccf23b6b6db09b9ad38e5d1d40ec
Saulo Lordão A. Barros <saulo.lordao@gmail.com>
2014-01-11 13:02:13 -0300

    Tipo algébrico para as transições.

---
+++ src/Transicao.hs
@@ -0,0 +1,22 @@
+module Transicao where
+
+data Transicao a e
+    = TransVazia e e
+    | Trans a e e
+    deriving (Eq, Ord, Show)
+
+origem, destino :: Transicao a e -> e
+
+origem (TransVazia o _) = o
+origem (Trans _ o _) = o
+
+destino (TransVazia _ d) = d
+destino (Trans _ _ d) = d
+
+entrada :: Transicao a e -> Maybe a
+entrada (Trans a _ _) = Just a
+entrada _ = Nothing
+
+mapT :: (e -> e) -> Transicao a e -> Transicao a e
+mapT f (TransVazia o d) = TransVazia (f o) (f d)
+mapT f (Trans e o d) = Trans e (f o) (f d)
